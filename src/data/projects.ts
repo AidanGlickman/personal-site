652a2da4f69877878dcc393df8856619ff58c1cb
Aidan Glickman <aidan@aidanglickman.com>
2023-03-19 23:41:29 -0500

    start on building projects tab

---
+++ src/data/projects.ts
@@ -0,0 +1,25 @@
+let projects = [
+    {
+        name: "Project 1",
+        description: "This is a project",
+        tags: ["tag1", "tag2"],
+        link: "https://www.google.com",
+        image: "/assets/example.jpeg"
+    },
+    {
+        name: "Project 2",
+        description: "This is another project",
+        tags: ["tag2", "tag3"],
+        link: "https://www.google.com",
+        image: "/assets/example.jpeg"
+    },
+    {
+        name: "Project 3",
+        description: "This is yet another project. This project has a very long description.",
+        tags: ["tag3", "tag4"],
+        link: "https://www.google.com",
+        image: "/assets/example.jpeg"
+    },
+]
+
+export default projects;
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,29 @@
+import { Card, Row, Col } from "react-bootstrap"
+import projects_data from "../data/projects"
+import "./Projects.scss"
+
 function Projects() {
     return (
         <div className="Projects">
             <h1>Projects</h1>
+            <Row xs={1} sm={2} md={3} lg={4} className="project-row g-4">
+                {projects_data.map((project, index) => (
+                    <Col key={index}>
+                        <Card bg="primary" className="project-card">
+                            <Card.Img variant="top" src={project.image} />
+                            <Card.Body>
+                                <Card.Title>{project.name}</Card.Title>
+                                <Card.Text className="desc">{project.description}</Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                                <small className="text-muted">
+                                    {project.tags}
+                                </small>
+                            </Card.Footer>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
         </div>
     )
 }
