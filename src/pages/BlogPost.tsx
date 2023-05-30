95ab77568934838d58f73a871c1db41099321ae0
Aidan Glickman <aidan@aidanglickman.com>
2023-05-30 15:40:35 -0400

    add unsolve blogpost, no blog discovery yet though

--- src/pages/BlogPost.tsx
+++ src/pages/BlogPost.tsx
@@ -2,21 +2,33 @@
 // Path: src/pages/BlogPost.tsx
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import './BlogPost.scss';
+import { BlogFrontmatter } from "../types";
 
 function BlogPost() {
     const { id } = useParams();
     const [Post, setPost] = useState((<div />));
+    const [FrontMatter, setFrontMatter] = useState({} as BlogFrontmatter);
     useEffect(() => {
         const fetchPost = async () => {
             const res = await import(`../blog/${id}.mdx`);
             setPost(res.default);
+            setFrontMatter(res.frontmatter as BlogFrontmatter);
         };
         fetchPost();
     }, [id]);
     return (
-        <React.Fragment>
-            {Post ? Post : <h1>Post not found</h1>}
-        </React.Fragment>
+        <div className='blogpost'>
+            {Post && FrontMatter ?
+                <React.Fragment>
+                    <h1 className="blog-title">{FrontMatter.title}</h1>
+                    <h3 className="blog-author">by Aidan Glickman</h3>
+                    <h5 className="blog-published">Published: {FrontMatter.datePublished}</h5>
+                    <hr />
+                    {Post}
+                </React.Fragment>
+                : <h1>Post not found</h1>}
+        </div>
     )
 }
 
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export interface BlogFrontmatter {
+    title: string;
+    seoTitle: string;
+    abstract: string;
+    isPublished: boolean;
+    datePublished: string;
+    tags: string[];
+}
